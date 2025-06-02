ea9c04a43768f4e46e4a68864477df0effdbde16
tanjilahamed99 <tanjil113355@gmail.com>
2025-06-02 23:04:11 +0600

    wish list page completed

--- src/app/(dashboard)/dashboard/wishlist/page.jsx
+++ src/app/(dashboard)/dashboard/wishlist/page.jsx
@@ -1,6 +1,11 @@
 import DashboardLayout from "@/components/dashboard/dashboard-layout"
 import Wishlist from "@/components/dashboard/wishlist"
 
+export const metadata = {
+    title: "Wishlist | Save Life",
+    description: "View and manage your wishlist",
+}
+
 export default function WishlistPage() {
 	return (
 		<DashboardLayout>
